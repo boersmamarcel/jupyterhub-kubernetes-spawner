3fea1f4957917e694a5725842fdcafaf2349cb2f
Brad Svee <sveesible@users.noreply.github.com>
2016-08-03 14:33:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name='kubespawner',
-    version='0.1',
-    description='JupyterHub Spawner targetting Kubernetes',
-    url='http://github.com/yuvipanda/jupyterhub-kubernetes-spawner',
-    author='Yuvi Panda',
-    author_email='yuvipanda@riseup.net',
-    license='BSD',
-    packages=['kubespawner'],
+    name='kubernetespawner',
+    version='1.0',
+    description='JupyterHub Spawner for Kubernetes',
+    url='http://github.com/sveesible/jupyterhub-kubernetes-spawner',
+    author='Brad Svee',
+    author_email='svee@google.com',
+    license='Apache 2.0',
+    packages=['kubernetespawner'],
 )
